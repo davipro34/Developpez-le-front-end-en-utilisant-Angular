23ce4e4ffc7d5fcc4bfbab4cee304090df474eb1
David Atlan <david@davipro.fr>
2024-01-13 12:35:29 +0100

    Update pie chart component

--- src/samples-files/pie-chart.ts
+++ src/samples-files/pie-chart.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { map, take } from 'rxjs/operators';
 import { Olympic } from 'src/app/core/models/Olympic';
-import { Participation } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -11,21 +9,13 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./pie-chart-sample.component.scss']
 })
 export class PieChartSampleComponent implements OnInit {
-  pieChartData$: Observable<any[]> = of([]);
+  public olympics$: Observable<Olympic[]> = of([]);
 
-  constructor (private olympicService: OlympicService) {}
+  constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
-    this.olympicService.getOlympics().pipe(
-      map((olympics: Olympic[]) => olympics.map((olympic: Olympic) => ({
-        name: olympic.country,
-        value: olympic.participations.reduce((total: number, p: Participation) => total + p.medalsCount, 0)
-      })))
-    ).subscribe((data: any[]) => {
-      this.pieChartData$ = of(data);
-    });
+    this.olympics$ = this.olympicService.getOlympicsForChart();
   }
-
   customColors = [
     {
       name: 'Italy',
--- src/samples-files/samples.ts
+++ src/samples-files/samples.ts
@@ -1,42 +1,32 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { catchError, tap, map, filter } from 'rxjs/operators';
-import { Olympic } from '../models/Olympic';
-import { Participation } from '../models/Participation';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class OlympicService {
-  private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<Olympic[]>([]);
 
-  constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
+    return this.http.get<any>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
+      tap(olympics => console.log(olympics)),
       catchError((error, caught) => {
         // TODO: improve error handling
         console.error(error);
         // can be useful to end loading state and let the user know something went wrong
         this.olympics$.next([]);
         return caught;
-      }),
+      })
     );
-  }
+
+
 
   // Get all datas
   getOlympics(): Observable<Olympic[]> {
-    return this.olympics$.asObservable().pipe(
-      filter(olympics$ => olympics$ && olympics$.length > 0)
-    );
+    return this.olympics$.asObservable();
   }
+
+
 
   // Get olympic by id for PieChartSample
   getOlympicsForChart(): Observable<any> {
     return this.getOlympics().pipe(
+      filter(olympics => olympics && olympics.length > 0), // ignore empty values
       tap(olympics => console.log(olympics)), // log the data
       map((olympics: Olympic[]) => olympics.map((olympic: Olympic) => ({
         name: olympic.country,
@@ -44,4 +34,21 @@ export class OlympicService {
       })))
     );
   }
-}
+
+  // Get olympic by country's id
+  getOlympicByCountryId(id: number): Observable<Olympic | undefined> {
+    return this.olympics$.pipe(
+      map((olympics: Olympic[]) =>
+        olympics.find((olympic: Olympic) => olympic.id === id)
+      )
+    );
+  }
+
+  // Get olympic by country's name
+  getOlympicByCountryName(name: string): Observable<Olympic | undefined> {
+    return this.olympics$.pipe(
+      map((olympics: Olympic[]) =>
+        olympics.find((olympic: Olympic) => olympic.country === name)
+      )
+    );
+  }
