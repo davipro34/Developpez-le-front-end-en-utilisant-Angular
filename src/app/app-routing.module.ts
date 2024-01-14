d097e367e41a17a5b930710a282d19f1e46a42e9
David Atlan <david@davipro.fr>
2024-01-14 19:54:16 +0100

    Add dynamic route for CountryDetails component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   },
   {
     path: 'CountryDetails',
+    // path: 'CountryDetails/:countryName', // TODO = Obtain countryName value from event (click) of pie chart
     component: CountryDetailsComponent
   },
   {
